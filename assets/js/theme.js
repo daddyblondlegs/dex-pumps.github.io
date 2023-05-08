cbe705f002a5aff75798f82a7d52e66956221362
daddyblondlegs <131564557+daddyblondlegs@users.noreply.github.com>
2023-05-08 05:20:11 +0000

    ok

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -309,3 +309,19 @@ window.onload = function () {
   imageMap.resize();
 }
 // Imape Map Resize (end)
+
+//Scroll to top
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
+//Scroll to top (end)
